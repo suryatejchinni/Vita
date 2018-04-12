6d715f475f8b92c67b5a23e165ace5e53af4de8d
suryatejchinni <surytejchinni@hotmail.com>
2018-04-12 15:55:11 +0530

    Initial commit

---
+++ public/backend/back.js
@@ -0,0 +1,157 @@
+// Initialize Firebase (ADD YOUR OWN DATA)
+var config = {
+    apiKey: "AIzaSyBNzBdIgVQtR-RfCrNveW8V9sEbR3JLqX8",
+    authDomain: "vita-7a279.firebaseapp.com",
+    databaseURL: "https://vita-7a279.firebaseio.com",
+    projectId: "vita-7a279",
+    storageBucket: "vita-7a279.appspot.com",
+    messagingSenderId: "874638990060"};
+  
+  firebase.initializeApp(config);
+  
+  // Reference messages collection
+  var firestore = firebase.firestore();
+  //const docRef = firestore.collection('registration').orderBy('company').limit(1);
+  
+  var downloadURL = 0;
+  var downloadURL2 = 0;
+  var downloadURL3 = 0;
+  var items = [];
+  var select = document.getElementById("selectNumber"); 
+  var img1 = document.getElementById("visit");
+  var img2 = document.getElementById("visit1");
+  var img3 = document.getElementById("visit2");
+
+
+  var name_s = document.getElementById('name');
+  var company_s = document.getElementById('company');
+  var email_s = document.getElementById('email');
+  var phone_s = document.getElementById('phone');
+  var message_s = document.getElementById('message');
+  var emptyref;
+  var url1_s,url2_s,url3_s;
+   
+  //document.getElementById('myForm').addEventListener('submit',viewForm);
+
+  function viewForm(e)
+  {
+    
+    var test = select.value;
+    console.log("id : "+test);
+    emptyref = citiesRef.doc(test);
+    emptyref.get()
+    .then(doc => {
+      if (!doc.exists) {
+        console.log('No such document!');
+      } else {
+        console.log('Document data:', doc.data());
+        url1_s =doc.data().url1;
+        url2_s =doc.data().url2;
+        url3_s =doc.data().url3;
+
+        img1.src=""+url1_s;
+        img2.src=""+url2_s;
+        img3.src=""+url3_s;
+        company_s.value=doc.data().company;
+        email_s.value=doc.data().email;
+        phone_s.value=doc.data().phone;
+        message_s.value=doc.data().message;
+        name_s.value=doc.data().name;
+
+       // img1.setAttribute
+      }
+    })
+    .catch(err => {
+      console.log('Error getting document', err);
+    });
+  }
+ 
+  var selectedValue;// = element.options[element.selectedIndex].value;
+
+  var citiesRef = firestore.collection('registration');
+  var allCities = citiesRef.get()
+      .then(snapshot => {
+          snapshot.forEach(doc => {
+              items.push(doc.id);
+              //console.log(items);
+              var el = document.createElement("option");
+              el.textContent = doc.data().name + ", "+doc.data().company;
+              el.value = doc.id;
+              select.appendChild(el);
+              //visit.setAttribute("src",""+doc.data().url1);
+          });
+      })
+      .catch(err => {
+          console.log('Error getting documents', err);
+      }); 
+  
+      
+
+    
+   
+  
+
+  
+  // Listen for form submit
+  document.getElementById('contactForm').addEventListener('submit', updateForm);
+  
+  // Submit form
+  function updateForm(e){
+    e.preventDefault();
+    console.log("In button");
+  
+    // Get values
+    var name = getInputVal('name');
+    var company = getInputVal('company');
+    var email = getInputVal('email');
+    var phone = getInputVal('phone');
+    var message = getInputVal('message');
+  
+    // Save message
+    saveMessage(name, company, email, phone, message);
+  
+    // Show alert
+    document.querySelector('.alert').style.display = 'block';
+  
+    // Hide alert after 3 seconds
+    setTimeout(function(){
+      document.querySelector('.alert').style.display = 'none';
+    },3000);
+  
+    // Clear form
+    document.getElementById('contactForm').reset();
+  }
+  
+  // Function to get get form values
+  function getInputVal(id){
+    return document.getElementById(id).value;
+  }
+  
+  // Save message to firebase
+  function saveMessage(name, company, email, phone, message){
+    emptyref.set({
+      name: name,
+      company:company,
+      email:email,
+      phone:phone,
+      message:message,
+      url1:url1_s,
+      url2:url2_s,
+      url3:url3_s
+    });
+  }
+
+
+  function openImg(imgs) {
+    console.log("In EXPAND");
+    // Get the expanded image
+    var expandImg = document.getElementById("expandedImg");
+    // Get the image text
+    var imgText = document.getElementById("imgtext");
+    // Use the same src in the expanded image as the image being clicked on from the grid
+    expandImg.src = imgs.src;
+    // Use the value of the alt attribute of the clickable image as text inside the expanded image
+    imgText.innerHTML = imgs.alt;
+    // Show the container element (hidden with CSS)
+    expandImg.parentElement.style.display = "block";
+  }
---
+++ public/main.js
@@ -0,0 +1,196 @@
+// Initialize Firebase (ADD YOUR OWN DATA)
+var config = {
+    apiKey: "AIzaSyBNzBdIgVQtR-RfCrNveW8V9sEbR3JLqX8",
+    authDomain: "vita-7a279.firebaseapp.com",
+    databaseURL: "https://vita-7a279.firebaseio.com",
+    projectId: "vita-7a279",
+    storageBucket: "vita-7a279.appspot.com",
+    messagingSenderId: "874638990060"};
+  
+  firebase.initializeApp(config);
+  
+  // Reference messages collection
+  var firestore = firebase.firestore();
+  const docRef = firestore.collection('registration');
+  var downloadURL = 0;
+  var downloadURL2 = 0;
+  var downloadURL3 = 0;
+  
+  
+  //Init photo uploads
+  
+  //Progress bars
+  var up1 = document.getElementById("up1");
+  var up2 = document.getElementById("up2");
+  var up3 = document.getElementById("up3");
+  
+  //Upload button
+  
+  var btn1 = document.getElementById("vis1");
+  var btn2 = document.getElementById("vis2");
+  var btn3 = document.getElementById("vis3");
+  
+  
+  // Listen for form submit
+  document.getElementById('contactForm').addEventListener('submit', submitForm);
+  btn1.addEventListener("change",function(e){
+  
+      //Get File
+      
+      var file1 = e.target.files[0];
+  
+      //Get Reference
+      var ref1 = firebase.storage().ref('photos/'+file1.name);
+  
+      //Upload the file
+      var uploadTask = ref1.put(file1);
+      //Update progress bar
+      uploadTask.on('state_changed', function(snapshot){
+        // Observe state change events such as progress, pause, and resume
+        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+        var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        up1.value=progress;
+        console.log('Upload is ' + progress + '% done');
+        switch (snapshot.state) {
+          case firebase.storage.TaskState.PAUSED: // or 'paused'
+            console.log('Upload is paused');
+            break;
+          case firebase.storage.TaskState.RUNNING: // or 'running'
+            console.log('Upload is running');
+            break;
+        }
+      }, function(error) {
+        // Handle unsuccessful uploads
+      }, function() {
+        // Handle successful uploads on complete
+        // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+        downloadURL = uploadTask.snapshot.downloadURL;
+        console.log(downloadURL);
+      });
+  
+  
+  
+  });
+  
+  btn2.addEventListener("change",function(e){
+  
+    //Get File
+    
+    var file2 = e.target.files[0];
+  
+    //Get Reference
+    var ref2 = firebase.storage().ref('photos/'+file2.name);
+  
+    //Upload the file
+    var uploadTask1 = ref2.put(file2);
+      //Update progress bar
+      uploadTask1.on('state_changed', function(snapshot){
+        // Observe state change events such as progress, pause, and resume
+        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+        var progress1 = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        up2.value=progress1;
+        console.log('Upload is ' + progress1 + '% done');
+        switch (snapshot.state) {
+          case firebase.storage.TaskState.PAUSED: // or 'paused'
+            console.log('Upload is paused');
+            break;
+          case firebase.storage.TaskState.RUNNING: // or 'running'
+            console.log('Upload is running');
+            break;
+        }
+      }, function(error) {
+        // Handle unsuccessful uploads
+      }, function() {
+        // Handle successful uploads on complete
+        // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+        downloadURL2 = uploadTask1.snapshot.downloadURL;
+        console.log(downloadURL);
+      });
+  });
+  
+  btn3.addEventListener("change",function(e){
+  
+    //Get File
+    
+    var file3 = e.target.files[0];
+  
+    //Get Reference
+    var ref3 = firebase.storage().ref('photos/'+file3.name);
+  
+  
+    //Upload the file
+    var task2 = ref3.put(file3);
+    //Update progress bar
+    var uploadTask3 = ref3.put(file3);
+      //Update progress bar
+      uploadTask3.on('state_changed', function(snapshot){
+        // Observe state change events such as progress, pause, and resume
+        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+        var progress2 = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        up3.value=progress2;
+        console.log('Upload is ' + progress2 + '% done');
+        switch (snapshot.state) {
+          case firebase.storage.TaskState.PAUSED: // or 'paused'
+            console.log('Upload is paused');
+            break;
+          case firebase.storage.TaskState.RUNNING: // or 'running'
+            console.log('Upload is running');
+            break;
+        }
+      }, function(error) {
+        // Handle unsuccessful uploads
+      }, function() {
+        // Handle successful uploads on complete
+        // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+        downloadURL3 = uploadTask3.snapshot.downloadURL;
+        console.log(downloadURL);
+      });
+  
+  
+  
+  });
+  // Submit form
+  function submitForm(e){
+    e.preventDefault();
+    console.log("In button");
+  
+    // Get values
+    var name = getInputVal('name');
+    var company = getInputVal('company');
+    var email = getInputVal('email');
+    var phone = getInputVal('phone');
+    var message = getInputVal('message');
+  
+    // Save message
+    saveMessage(name, company, email, phone, message,downloadURL,downloadURL2,downloadURL3);
+  
+    // Show alert
+    document.querySelector('.alert').style.display = 'block';
+  
+    // Hide alert after 3 seconds
+    setTimeout(function(){
+      document.querySelector('.alert').style.display = 'none';
+    },3000);
+  
+    // Clear form
+    document.getElementById('contactForm').reset();
+  }
+  
+  // Function to get get form values
+  function getInputVal(id){
+    return document.getElementById(id).value;
+  }
+  
+  // Save message to firebase
+  function saveMessage(name, company, email, phone, message,url1,url2,url3){
+    docRef.add({
+      name: name,
+      company:company,
+      email:email,
+      phone:phone,
+      message:message,
+      url1:url1,
+      url2:url2,
+      url3:url3,
+    });
+  }
